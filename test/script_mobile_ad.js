0dd546d1e3585a4dc7126254825201d7c31c5c7b
Laurence Colombie <laurencecolombie@Laurences-MBP.home>
2015-01-16 17:46:31 +0000

    scroll

--- test/script_mobile_ad.js
+++ test/script_mobile_ad.js
@@ -1,18 +1,27 @@
 
 var KONBINIMOBILEAD = (function () {
 
+	var ad_container = document.getElementById('5196125');
+	
+	ad_container.style.width = '100%';
+	ad_container.style.height = '200px';
+	ad_container.style.backgroundImage = 'url(test.jpg)';
+
 	//if element is an image
-	var image = document.createElement('img'),
-		articles = $("article.item");
+	// var image = document.createElement('img'),
+	// 	articles = $("article.item");
 		
-		for ( var i = 0; i < articles.length; i++ ) {
-			var article = articles[i];
-			//remove bottom margin
-			article.style.marginBottom = "0px";
+	// 	for ( var i = 0; i < articles.length; i++ ) {
+	// 		var article = articles[i];
+	// 		//remove bottom margin
+	// 		article.style.marginBottom = "0px";
 			
-		}
+	// 	}
 
 	return {
+		appendImage:function(node) {
+			node.appendChild(image);
+		},
 		insertImageBefore:function(node) {
 
 			//console.log($("article.item").length);
@@ -33,5 +42,6 @@ var KONBINIMOBILEAD = (function () {
 })();
 
 
-KONBINIMOBILEAD.insertImageBefore(document.getElementById('testMobileAd'));
-KONBINIMOBILEAD.setImageAttributes('test.jpg','test image');
+//KONBINIMOBILEAD.appendImage(document.getElementById('wrapper'));
+//KONBINIMOBILEAD.insertImageBefore(document.getElementById('testMobileAd'));
+//KONBINIMOBILEAD.setImageAttributes('test.jpg','test image');
