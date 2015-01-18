204696d82789e5d63bccf4b0049db025590e2fbc
Laurence Colombie <laurencecolombie@Laurences-MBP.lan>
2015-01-18 14:58:52 +0000

    delire

--- test/iscroll2.js
+++ test/iscroll2.js
@@ -433,97 +433,97 @@ IScroll.prototype = {
 	_move: function (e) {
 
 		//console.log(alert('move'));
-		// if ( !this.enabled || utils.eventType[e.type] !== this.initiated ) {
-		// 	return;
-		// }
+		if ( !this.enabled || utils.eventType[e.type] !== this.initiated ) {
+			return;
+		}
 
-		// if ( this.options.preventDefault ) {	// increases performance on Android? TODO: check!
-		// 	e.preventDefault();
-		// }
+		if ( this.options.preventDefault ) {	// increases performance on Android? TODO: check!
+			e.preventDefault();
+		}
 
-// 		var point		= e.touches ? e.touches[0] : e,
-// 			deltaX		= point.pageX - this.pointX,
-// 			deltaY		= point.pageY - this.pointY,
-// 			timestamp	= utils.getTime(),
-// 			newX, newY,
-// 			absDistX, absDistY;
+		var point		= e.touches ? e.touches[0] : e,
+			deltaX		= point.pageX - this.pointX,
+			deltaY		= point.pageY - this.pointY,
+			timestamp	= utils.getTime(),
+			newX, newY,
+			absDistX, absDistY;
 
-// 		this.pointX		= point.pageX;
-// 		this.pointY		= point.pageY;
+		this.pointX		= point.pageX;
+		this.pointY		= point.pageY;
 
-// 		this.distX		+= deltaX;
-// 		this.distY		+= deltaY;
-// 		absDistX		= Math.abs(this.distX);
-// 		absDistY		= Math.abs(this.distY);
+		this.distX		+= deltaX;
+		this.distY		+= deltaY;
+		absDistX		= Math.abs(this.distX);
+		absDistY		= Math.abs(this.distY);
 
-// 		// We need to move at least 10 pixels for the scrolling to initiate
-// 		if ( timestamp - this.endTime > 300 && (absDistX < 10 && absDistY < 10) ) {
-// 			return;
-// 		}
+		// We need to move at least 10 pixels for the scrolling to initiate
+		if ( timestamp - this.endTime > 300 && (absDistX < 10 && absDistY < 10) ) {
+			return;
+		}
 
-// 		// If you are scrolling in one direction lock the other
-// 		if ( !this.directionLocked && !this.options.freeScroll ) {
-// 			if ( absDistX > absDistY + this.options.directionLockThreshold ) {
-// 				this.directionLocked = 'h';		// lock horizontally
-// 			} else if ( absDistY >= absDistX + this.options.directionLockThreshold ) {
-// 				this.directionLocked = 'v';		// lock vertically
-// 			} else {
-// 				this.directionLocked = 'n';		// no lock
-// 			}
-// 		}
+		// If you are scrolling in one direction lock the other
+		if ( !this.directionLocked && !this.options.freeScroll ) {
+			if ( absDistX > absDistY + this.options.directionLockThreshold ) {
+				this.directionLocked = 'h';		// lock horizontally
+			} else if ( absDistY >= absDistX + this.options.directionLockThreshold ) {
+				this.directionLocked = 'v';		// lock vertically
+			} else {
+				this.directionLocked = 'n';		// no lock
+			}
+		}
 
-// 		if ( this.directionLocked == 'h' ) {
-// 			if ( this.options.eventPassthrough == 'vertical' ) {
-// 				//e.preventDefault();
-// 			} else if ( this.options.eventPassthrough == 'horizontal' ) {
-// 				this.initiated = false;
-// 				return;
-// 			}
+		if ( this.directionLocked == 'h' ) {
+			if ( this.options.eventPassthrough == 'vertical' ) {
+				//e.preventDefault();
+			} else if ( this.options.eventPassthrough == 'horizontal' ) {
+				this.initiated = false;
+				return;
+			}
 
-// 			deltaY = 0;
-// 		} else if ( this.directionLocked == 'v' ) {
-// 			if ( this.options.eventPassthrough == 'horizontal' ) {
-// 				e.preventDefault();
-// 			} else if ( this.options.eventPassthrough == 'vertical' ) {
-// 				this.initiated = false;
-// 				return;
-// 			}
+			deltaY = 0;
+		} else if ( this.directionLocked == 'v' ) {
+			if ( this.options.eventPassthrough == 'horizontal' ) {
+				e.preventDefault();
+			} else if ( this.options.eventPassthrough == 'vertical' ) {
+				this.initiated = false;
+				return;
+			}
 
-// 			deltaX = 0;
-// 		}
+			deltaX = 0;
+		}
 
-// 		deltaX = this.hasHorizontalScroll ? deltaX : 0;
-// 		deltaY = this.hasVerticalScroll ? deltaY : 0;
+		deltaX = this.hasHorizontalScroll ? deltaX : 0;
+		deltaY = this.hasVerticalScroll ? deltaY : 0;
 
-// 		newX = this.x + deltaX;
-// 		newY = this.y + deltaY;
+		newX = this.x + deltaX;
+		newY = this.y + deltaY;
 
-// 		// Slow down if outside of the boundaries
-// 		if ( newX > 0 || newX < this.maxScrollX ) {
-// 			newX = this.options.bounce ? this.x + deltaX / 3 : newX > 0 ? 0 : this.maxScrollX;
-// 		}
-// 		if ( newY > 0 || newY < this.maxScrollY ) {
-// 			newY = this.options.bounce ? this.y + deltaY / 3 : newY > 0 ? 0 : this.maxScrollY;
-// 		}
+		// Slow down if outside of the boundaries
+		if ( newX > 0 || newX < this.maxScrollX ) {
+			newX = this.options.bounce ? this.x + deltaX / 3 : newX > 0 ? 0 : this.maxScrollX;
+		}
+		if ( newY > 0 || newY < this.maxScrollY ) {
+			newY = this.options.bounce ? this.y + deltaY / 3 : newY > 0 ? 0 : this.maxScrollY;
+		}
 
-// 		this.directionX = deltaX > 0 ? -1 : deltaX < 0 ? 1 : 0;
-// 		this.directionY = deltaY > 0 ? -1 : deltaY < 0 ? 1 : 0;
+		this.directionX = deltaX > 0 ? -1 : deltaX < 0 ? 1 : 0;
+		this.directionY = deltaY > 0 ? -1 : deltaY < 0 ? 1 : 0;
 
-// 		if ( !this.moved ) {
-// 			this._execEvent('scrollStart');
-// 		}
+		if ( !this.moved ) {
+			this._execEvent('scrollStart');
+		}
 
-// 		this.moved = true;
+		this.moved = true;
 
-// 		this._translate(newX, newY);
+		this._translate(newX, newY);
 
-// /* REPLACE START: _move */
+/* REPLACE START: _move */
 
-// 		if ( timestamp - this.startTime > 300 ) {
-// 			this.startTime = timestamp;
-// 			this.startX = this.x;
-// 			this.startY = this.y;
-// 		}
+		if ( timestamp - this.startTime > 300 ) {
+			this.startTime = timestamp;
+			this.startX = this.x;
+			this.startY = this.y;
+		}
 
 /* REPLACE END: _move */
 
