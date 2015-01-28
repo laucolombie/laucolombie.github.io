139f7e5173c616c5a7577426d579c4532fa006d8
Laurence Colombie <laurencecolombie@Laurences-MBP.home>
2015-01-28 18:16:01 +0000

    new images

--- test/KonbiniAd.js
+++ test/KonbiniAd.js
@@ -1,26 +1,14 @@
 /**
  * @module KonbiniAd
  **/
-(function (window, document, navigator) {
+(function (window, document, Math) {
 
-	//"use strict";
-
-	//size properties for responsive support
-	var MAX_MOBILE_DEVICE_SIZE = {
-			portrait:{
-				w:414,h:736
-			},
-			landscape: {
-				w:736,h:414
-			}
-		},
-		MAX_TABLET_DEVICE_SIZE = {},
-		MIN_DESKTOP_DEVICE_SIZE = {};
+	"use strict";
 
 	var utils = (function () {
 
 		var utility = {};
-		
+
 		utility.extend = function (target, obj) {
 			for ( var i in obj ) {
 				target[i] = obj[i];
@@ -50,9 +38,11 @@
 		};
 		utility.addCSSRule = function(sheet, selector, rules, index) {
 			if("insertRule" in sheet) {
+				console.log("insertRule");
 				sheet.insertRule(selector + "{" + rules + "}", index);
 			}
 			else if("addRule" in sheet) {
+				console.log("addRule");
 				sheet.addRule(selector, rules, index);
 			}
 		};
@@ -82,8 +72,6 @@
 
 	function KonbiniAd(name,parentNode,options,clicktag) {
 
-		console.log(Math);
-
 		this.imgWrapper = document.createElement('div');
 		this.openArea = document.createElement('div');
 		this.openAreaCloseButton = document.createElement('div');
@@ -111,8 +99,6 @@
 			return parentNode;
 		};
 
-
-
 	}
 
 	KonbiniAd.prototype = {
@@ -138,7 +124,7 @@
 			this.addCloseButton();
 			this.addOpenButton();
 
-			//ADAPT LAYOUT TO PAGE - NOT NICE AT ALL AS HARD CODED
+			//ADAPT LAYOUT TO PAGE
 			document.getElementById('wrapper').style.background = 'transparent';
 		    document.getElementById('wrapper').style.display = 'block';
 		    document.getElementById('wrapper').style.overflow = 'hidden';
@@ -149,66 +135,18 @@
 		},
 
 		setWallpaperImage: function(images) {
-
 			this.images = images;
-			
+			this.applyImage();
 			utils.addCSSRule(this.styleSheet,"#ad_image","position:fixed; background-color:transparent; display:block; overflow: hidden");
 			//HERE I AM GOING TO MANIPULATE THE POSITION OF THE IMAGE
-	  		//get viewport dimensions
-			
-	  		//detect if portrait mode
-			if (utils.isPortrait.matches) {
-				
-				if (utils.viewport().width <= MAX_MOBILE_DEVICE_SIZE.portrait.w) {
-
-					var cropWidth = MAX_MOBILE_DEVICE_SIZE.landscape.w - utils.viewport().width;
-					
-					var ratio = Math.round((utils.viewport().width/MAX_MOBILE_DEVICE_SIZE.landscape.w) * 100);
-
-					this.imgWrapper.style.minWidth =  "100%";
-					this.imgWrapper.style.minHeight =  utils.viewport().height + "px";
-
-					/* Set up proportionate scaling */
-					this.imgWrapper.style.width =  "auto";
-					this.imgWrapper.style.height =  "100%";
-
-
-	
-  
-	
-  
-					/* Set up positioning */
-
-
-					// this.imgWrapper.style.width =  utils.viewport().width + "px";
-					// this.imgWrapper.style.height =  utils.viewport().height + "px";
-					this.imgWrapper.style.backgroundSize = "cover";
-
-
-					
-					// this.imgWrapper.style.minHeight = "100%";
+	  		this.imgWrapper.style.width =  "100%";
+	  		this.imgWrapper.style.backgroundSize = "cover";
+	  		this.imgWrapper.style.height = "100%";
 	  		this.imgWrapper.style.left = '0px';
 	  		this.imgWrapper.style.top = '0px';
-		  	// this.imgWrapper.style.backgroundPosition = '0px 0px';
-		  	// this.imgWrapper.style.backgroundRepeat = 'no-repeat no-repeat';
-		  	// this.imgWrapper.style.backgroundAttachment = 'scroll';
-
-					//console.log((736 - wt) / MAX_MOBILE_DEVICE_SIZE.landscape.w);
-
-
-				}
-			} 
-
-			
-
-	  		// this.imgWrapper.style.width =  "100%";
-	  		// this.imgWrapper.style.backgroundSize = "100%";
-	  		//this.imgWrapper.style.minHeight = "100%";
-	  		// this.imgWrapper.style.left = '0px';
-	  		// this.imgWrapper.style.top = '0px';
-		  	// this.imgWrapper.style.backgroundPosition = '0px 0px';
-		  	// this.imgWrapper.style.backgroundRepeat = 'no-repeat no-repeat';
-		  	// this.imgWrapper.style.backgroundAttachment = 'scroll';
+		  	this.imgWrapper.style.backgroundPosition = '0px 0px';
+		  	this.imgWrapper.style.backgroundRepeat = 'no-repeat no-repeat';
+		  	this.imgWrapper.style.backgroundAttachment = 'scroll';
 		  	
 		},
 
@@ -228,6 +166,7 @@
 
 		},
 		resize: function() {	
+
 			this.applyImage();
 			//get the width and height of the viewport:
 			if (utils.hasJQuery) {
@@ -238,10 +177,11 @@
 			}
 		},
 		applyImage: function() {
+			
 			if (utils.isPortrait.matches) {
-				this.imgWrapper.style.background = 'url("' + this.images.urlm + '") no-repeat center center fixed';
+				this.imgWrapper.style.backgroundImage = 'url("' + this.images.urlmp + '")';
 			} else {
-				this.imgWrapper.style.background = 'url("' + this.images.urlm + '") no-repeat center center fixed';
+				this.imgWrapper.style.backgroundImage = 'url("' + this.images.urlml + '")';
 			}
 		},
 		handleEvent: function (e) {
@@ -350,6 +290,7 @@
 	window.KonbiniAd = KonbiniAd;
 
 })(window, document, Math);
+
 
 
 
